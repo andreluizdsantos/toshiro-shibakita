326da812fd3be0d16e712a0c4b7f37430b31de82
andreluizdsantos <andreluizdsantos@gmail.com>
2022-08-11 19:32:18 -0300

    Add Dockerfiles and Deployment

--- src/index.php
+++ src/index.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$servername = "mysql";
+$username = "toshiro";
+$password = "Jaspion#135@";
+$database = "toshiro_db";
 
 // Criar conexão
 
